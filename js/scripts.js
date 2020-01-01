554e924fcdc2116b096e18e1dc3b9e80050dd1b2
Manas <prometheansacrifice@gmail.com>
2020-01-01 20:11:14 +0530

    Basic content

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+let animatedText = `
+          <div class="rb-animated-content">
+            <div class="content__container">
+              <p class="content__container__text">
+                Reason
+              </p>
+              <ul class="content__container__list">
+                <li class="content__container__list__item">Bangalore</li>
+                <li class="content__container__list__item">ಬೆಂಗಳೂರು</li>
+              </ul>
+            </div>
+          </div>
+`;
+
+window.addEventListener("DOMContentLoaded", _e => {
+  document.querySelector("h1").innerHTML = animatedText;
+});
